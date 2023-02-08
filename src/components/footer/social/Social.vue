89b4a18e869747a03119fc830947ef7a77cab856
Santo Khan <santokhan1999@gmail.com>
2023-02-08 19:06:51 +0600

    Update logo padding

--- src/components/footer/social/Social.vue
+++ src/components/footer/social/Social.vue
@@ -4,8 +4,11 @@ import Link from './Link.vue';
 
 <template>
     <div class="tw-flex tw-justify-start tw-items-center tw-gap-10 tw-lg:gap-16 tw-text-2xl tw-py-6">
-        <Link href="https://facebook.com/GoXupport"><i class="fab fa-facebook" aria-hidden="true"></i></Link>
-        <Link href="https://instagram.com/GoXupport"><i class="fab fa-instagram" aria-hidden="true"></i></Link>
-        <Link href="https://twitter.com/GoXupport"><i class="fab fa-twitter" aria-hidden="true"></i></Link>
+        <Link href="https://www.facebook.com/profile.php?id=100089147406118&mibextid=ZbWKwL" target="_blank"><i
+            class="fab fa-facebook" aria-hidden="true"></i></Link>
+        <Link href="https://instagram.com/GoXupport" target="_blank"><i class="fab fa-instagram" aria-hidden="true"></i>
+        </Link>
+        <Link href="https://twitter.com/GoXupport" target="_blank"><i class="fab fa-twitter" aria-hidden="true"></i>
+        </Link>
     </div>
 </template>
