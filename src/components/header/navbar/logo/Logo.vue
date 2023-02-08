14940e13f55b107f0e1acab7cb16d8f9d136d6d7
Santo Khan <santokhan1999@gmail.com>
2023-02-08 14:27:34 +0600

    Logo

--- src/components/header/navbar/logo/Logo.vue
+++ src/components/header/navbar/logo/Logo.vue
@@ -4,7 +4,7 @@ import LogoImg from '../../../../assets/images/logo.png'
 </script>
 
 <template>
-    <RouterLink to="/" style="height: 48px;" class="navbar-brand js-scroll-trigger d-flex items-center gap-2">
+    <RouterLink to="/" style="height: 44px;" class="navbar-brand js-scroll-trigger d-flex items-center gap-2">
         <img style="width: 100%;" :src="LogoImg" alt="logo" class="tw-object-contain">
     </RouterLink>
 </template>
